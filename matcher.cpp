Davidebyzero/RegexMathEngine

commit 9d149935521302fe50c44819079fd24c5819abc0

debugTrace: Show when individual non-matches happen

matcher.cpp
@@ -15,6 +15,9 @@
 template <bool USE_STRINGS>
 void RegexMatcher<USE_STRINGS>::nonMatch()
 {
+    if (debugTrace)
+        fputs(": non-match", stderr);
+
     position = groupStackTop->position;
 
     for (;;)
@@ -1192,7 +1195,7 @@ bool RegexMatcher<USE_STRINGS>::Match(RegexGroup &regex, Uint numCaptureGroups, 
 
         match = 0;
 
-        do
+        for (;;)
         {
             RegexSymbol *thisSymbol = *symbol;
             if (!thisSymbol) // exiting a group?
@@ -1347,22 +1350,37 @@ bool RegexMatcher<USE_STRINGS>::Match(RegexGroup &regex, Uint numCaptureGroups, 
                 }
                 for (GroupStackNode *i=groupStackTop; i>groupStackBase; i--)
                     fputc(')', stderr);
-                fputc('\n', stderr);
 
-                fputc('\n', stderr);
                 numSteps++;
             }
-            (this->*matchFunction(thisSymbol))(thisSymbol);
+
+            (this->*matchFunction(thisSymbol))(thisSymbol); // in debugTrace mode, nonMatch() will print that there was a non-match
+
+            if (debugTrace)
+                fputc('\n', stderr);
+
+            if (match)
+                break;
+
+            if (debugTrace)
+                fputc('\n', stderr);
         }
-        while (!match);
 
         stack.flush();
 
-        if (match > 0)
+        if (match > 0 || anchored)
+        {
+            if (debugTrace)
+                fputc('\n', stderr);
             break;
-
-        if (anchored)
-            break;
+        }
+        if (debugTrace)
+        {
+            fputs("No match found", stderr);
+            if (curPosition+1 <= input)
+                fprintf(stderr, "; trying at {%llu}", curPosition+1);
+            fputs("\n\n", stderr);
+        }
     }
 
     returnMatchOffset = curPosition;

commit 89dfee9c76cd12ce93e09b64fa8c481d8bd24e4c

debugTrace: Fix debug output when exiting negative lookahead
If a match was found inside a negative lookahead, resulting in a non-match ouside it, this was incorrectly printed in the debugTrace output as ": non-match" with a missing newline.

Bug introduced in 9d149935521302fe50c44819079fd24c5819abc0

matcher.cpp
@@ -13,10 +13,15 @@
 #include "matcher.h"
 
 template <bool USE_STRINGS>
-void RegexMatcher<USE_STRINGS>::nonMatch()
+void RegexMatcher<USE_STRINGS>::nonMatch(bool negativeLookahead)
 {
     if (debugTrace)
-        fputs(": non-match", stderr);
+    {
+        if (negativeLookahead)
+            fputs("Match found inside negative lookahead, resulting in a non-match outside it\n\n", stderr);
+        else
+            fputs(": non-match", stderr);
+    }
 
     position = groupStackTop->position;
 
@@ -1268,7 +1273,7 @@ bool RegexMatcher<USE_STRINGS>::Match(RegexGroup &regex, Uint numCaptureGroups, 
                     while (groupStackTop >= groupStackOldTop);
 
                     // if we've reached here, it means a match was found inside the negative lookahead, which makes it a non-match outside
-                    nonMatch();
+                    nonMatch(true);
                     continue;
                 }
 

matcher.h
@@ -143,7 +143,7 @@ class RegexMatcher : public RegexMatcherBase<USE_STRINGS>
     char match; // zero = looking for match, negative = match failed, positive = match found
     bool anchored; // indicates whether we can optimize the search by only trying a match at the start
 
-    void nonMatch();
+    void nonMatch(bool negativeLookahead = false);
     void yesMatch(Uint64 newPosition, bool haveChoice);
     void pushStack();
     void enterGroup(RegexGroup *group);