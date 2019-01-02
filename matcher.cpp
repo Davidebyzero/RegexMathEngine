Davidebyzero/RegexMathEngine

commit 33053ae26f685ebef7d503cc1b5486778ddc8e6f

debugTrace: Include contents of string captures in USE_STRINGS mode
Previously only the sizes of captures were printed, which is all the information necessary in numeric mode, but not enough in strings mode.

matcher.cpp
@@ -1321,8 +1321,7 @@ bool RegexMatcher<USE_STRINGS>::Match(RegexGroup &regex, Uint numCaptureGroups, 
 #else
                 fprintf(stderr, "Step %llu: {%llu|%llu} ", numSteps, position, input - position);
 #endif
-                for (Uint *i=captureStackBase; i<captureStackTop; i++)
-                    fprintf(stderr, "\\%u=%llu%s", *i+1, captures[*i], i<captureStackTop-1 ? ", " : "");
+                fprintCaptures(stderr);
                 fputc('\n', stderr);
 
                 for (GroupStackNode *i=groupStackBase; i<=groupStackTop; i++)
@@ -1370,6 +1369,34 @@ bool RegexMatcher<USE_STRINGS>::Match(RegexGroup &regex, Uint numCaptureGroups, 
     returnMatchLength = (size_t)(position - curPosition);
     
     return match > 0;
+}
+
+void RegexMatcher<false>::fprintCaptures(FILE *f)
+{
+    for (Uint *i=captureStackBase; i<captureStackTop; i++)
+        fprintf(stderr, "\\%u=%llu%s", *i+1, captures[*i], i<captureStackTop-1 ? ", " : "");
+}
+
+void RegexMatcher<true>::fprintCaptures(FILE *f)
+{
+    for (Uint *i=captureStackBase; i<captureStackTop; i++)
+    {
+        fprintf(stderr, "\\%u=\"", *i+1);
+        const char *s = captureOffsets[*i];
+        for (Uint64 len=captures[*i]; len!=0; len--)
+        {
+            switch (*s)
+            {
+            case '\\': fputs("\\\\", stderr); break;
+            case '"':  fputs("\\\"", stderr); break;
+            default:
+                fputc(*s, stderr);
+                break;
+            }
+            s++;
+        }
+        fprintf(stderr, "\" (%llu:%llu)%s", captureOffsets[*i] - stringToMatchAgainst, captures[*i], i<captureStackTop-1 ? ", " : "");
+    }
 }
 
 template bool RegexMatcher<false>::Match(RegexGroup &regex, Uint numCaptureGroups, Uint maxGroupDepth, Uint64 _input, Uint64 &returnMatchOffset, Uint64 &returnMatchLength);

matcher.h
@@ -200,6 +200,8 @@ class RegexMatcher : public RegexMatcherBase<USE_STRINGS>
     inline void (RegexMatcher<USE_STRINGS>::*chooseBuiltinCharacterClassFunction(bool (*characterMatchFunction)(Uchar ch), void (RegexMatcher<USE_STRINGS>::*matchFunction)(RegexSymbol *thisSymbol)))(RegexSymbol *thisSymbol);
     inline void virtualizeSymbols(RegexGroup *rootGroup);
 
+    inline void fprintCaptures(FILE *f);
+
 public:
     inline RegexMatcher();
     inline ~RegexMatcher();

commit ae854b39e404f0c9d12f589428909a7cc65718c0

minor fix: Make fprintCaptures() use its file parameter
Since only stderr is passed to it, it makes no difference as currently used.

Minor bug introduced in 33053ae26f685ebef7d503cc1b5486778ddc8e6f

matcher.cpp
@@ -1404,28 +1404,28 @@ bool RegexMatcher<USE_STRINGS>::Match(RegexGroup &regex, Uint numCaptureGroups, 
 void RegexMatcher<false>::fprintCaptures(FILE *f)
 {
     for (Uint *i=captureStackBase; i<captureStackTop; i++)
-        fprintf(stderr, "\\%u=%llu%s", *i+1, captures[*i], i<captureStackTop-1 ? ", " : "");
+        fprintf(f, "\\%u=%llu%s", *i+1, captures[*i], i<captureStackTop-1 ? ", " : "");
 }
 
 void RegexMatcher<true>::fprintCaptures(FILE *f)
 {
     for (Uint *i=captureStackBase; i<captureStackTop; i++)
     {
-        fprintf(stderr, "\\%u=\"", *i+1);
+        fprintf(f, "\\%u=\"", *i+1);
         const char *s = captureOffsets[*i];
         for (Uint64 len=captures[*i]; len!=0; len--)
         {
             switch (*s)
             {
-            case '\\': fputs("\\\\", stderr); break;
-            case '"':  fputs("\\\"", stderr); break;
+            case '\\': fputs("\\\\", f); break;
+            case '"':  fputs("\\\"", f); break;
             default:
-                fputc(*s, stderr);
+                fputc(*s, f);
                 break;
             }
             s++;
         }
-        fprintf(stderr, "\" (%llu:%llu)%s", captureOffsets[*i] - stringToMatchAgainst, captures[*i], i<captureStackTop-1 ? ", " : "");
+        fprintf(f, "\" (%llu:%llu)%s", captureOffsets[*i] - stringToMatchAgainst, captures[*i], i<captureStackTop-1 ? ", " : "");
     }
 }
 