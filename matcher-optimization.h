Davidebyzero/RegexMathEngine

commit 2ed9b8424a4d6193d956c68d094140b6459025ac

[internal] Move matcher optimization into a separate source code file

matcher.h
@@ -215,12 +215,15 @@ class RegexMatcher : public RegexMatcherBase<USE_STRINGS>
     void matchSymbol_IsPowerOf2           (RegexSymbol *thisSymbol);
 
     template <typename MATCH_TYPE>
+    inline bool runtimeOptimize_matchSymbol_Character_or_Backref(RegexSymbol *const thisSymbol, Uint64 const multiple, MATCH_TYPE const repetend);
+    template <typename MATCH_TYPE>
     void matchSymbol_Character_or_Backref (RegexSymbol *thisSymbol, Uint64 multiple, MATCH_TYPE pBackref);
 
     inline void (RegexMatcher<USE_STRINGS>::*&matchFunction(RegexSymbol *thisSymbol))(RegexSymbol *thisSymbol);
     inline bool characterCanMatch(RegexSymbol *thisSymbol);
     inline bool8 characterClassCanMatch(RegexCharacterClass *thisSymbol);
     inline void (RegexMatcher<USE_STRINGS>::*chooseBuiltinCharacterClassFunction(bool (*characterMatchFunction)(Uchar ch), void (RegexMatcher<USE_STRINGS>::*matchFunction)(RegexSymbol *thisSymbol)))(RegexSymbol *thisSymbol);
+    inline bool staticallyOptimizeGroup(RegexSymbol **thisSymbol);
     inline void virtualizeSymbols(RegexGroup *rootGroup);
 
     inline void fprintCapture(FILE *f, Uint i);

Makefile
@@ -17,6 +17,6 @@ $(BIN):\
 	$(OBJ) 
 	$(CPP) $(CFLAGS) -o $@ $(OBJ)
 
-$(OBJ): matcher.h parser.h regex.h tools.h
+$(OBJ): matcher.h matcher-optimization.h parser.h regex.h tools.h
 
 clean:; rm -f $(OBJ) $(BIN) core

regex.vcxproj
@@ -279,6 +279,7 @@
     <ClCompile Include="tools.cpp" />
   </ItemGroup>
   <ItemGroup>
+    <ClInclude Include="matcher-optimization.h" />
     <ClInclude Include="matcher.h" />
     <ClInclude Include="parser.h" />
     <ClInclude Include="regex.h" />

commit e28e3ebbcd59dfbdcaa295e2f261afc0d15ad96f

Allow project to compile with GCC again
* Replace __forceinline with a macro (broken in 2ed9b8424a4d6193d956c68d094140b6459025ac)
* Replace __assume(0) with a macro (broken in 568de4e8b30548afd985acc2debfa460e3abf7a5)
* Use "this->" to access some members (broken in 54ac2f9cfdef352c6b7c23021abdc8126ae41a1a, 568de4e8b30548afd985acc2debfa460e3abf7a5)
* Befriend a class that needed it (broken in 54ac2f9cfdef352c6b7c23021abdc8126ae41a1a)
* Explicitly specify template parameters when needed (broken in 799837e32e914d31345831f46f36ace9ded91252)

matcher-optimization.h
@@ -1,5 +1,5 @@
 template <bool USE_STRINGS>
-__forceinline bool RegexMatcher<USE_STRINGS>::staticallyOptimizeGroup(RegexSymbol **thisSymbol)
+ALWAYS_INLINE bool RegexMatcher<USE_STRINGS>::staticallyOptimizeGroup(RegexSymbol **thisSymbol)
 // return true if the group has been rewritten into a specialized symbol
 {
     if (optimizationLevel >= 2)
@@ -70,7 +70,7 @@ __forceinline bool RegexMatcher<USE_STRINGS>::staticallyOptimizeGroup(RegexSymbo
 
 template <bool USE_STRINGS>
 template <typename MATCH_TYPE>
-__forceinline bool RegexMatcher<USE_STRINGS>::runtimeOptimize_matchSymbol_Character_or_Backref(RegexSymbol *const thisSymbol, Uint64 const multiple, MATCH_TYPE const repetend)
+ALWAYS_INLINE bool RegexMatcher<USE_STRINGS>::runtimeOptimize_matchSymbol_Character_or_Backref(RegexSymbol *const thisSymbol, Uint64 const multiple, MATCH_TYPE const repetend)
 // return true if this optimizer function handled the match and the caller should do nothing further
 {
     if (optimizationLevel && !thisSymbol->possessive)

matcher.h
@@ -606,7 +606,7 @@ class Backtrack_SkipGroup : public BacktrackNode<USE_STRINGS>
     }
     virtual captureTuple popForAtomicForwardCapture(RegexMatcher<USE_STRINGS> &matcher, Uint captureNum)
     {
-        __assume(0);
+        UNREACHABLE_CODE;
     }
     virtual bool okayToTryAlternatives(RegexMatcher<USE_STRINGS> &matcher)
     {
@@ -661,7 +661,7 @@ class Backtrack_EnterGroup : public BacktrackNode<USE_STRINGS>
     }
     virtual captureTuple popForAtomicForwardCapture(RegexMatcher<USE_STRINGS> &matcher, Uint captureNum)
     {
-        __assume(0);
+        UNREACHABLE_CODE;
     }
     virtual bool okayToTryAlternatives(RegexMatcher<USE_STRINGS> &matcher)
     {
@@ -693,7 +693,7 @@ class Backtrack_BeginAtomicGroup : public BacktrackNode<USE_STRINGS>
     }
     virtual captureTuple popForAtomicForwardCapture(RegexMatcher<USE_STRINGS> &matcher, Uint captureNum)
     {
-        __assume(0);
+        UNREACHABLE_CODE;
     }
     virtual bool okayToTryAlternatives(RegexMatcher<USE_STRINGS> &matcher)
     {
@@ -745,7 +745,7 @@ class Backtrack_LeaveMolecularLookahead : public BacktrackNode<USE_STRINGS>
     }
     virtual captureTuple popForAtomicForwardCapture(RegexMatcher<USE_STRINGS> &matcher, Uint captureNum)
     {
-        __assume(0);
+        UNREACHABLE_CODE;
     }
     virtual bool okayToTryAlternatives(RegexMatcher<USE_STRINGS> &matcher)
     {
@@ -830,7 +830,7 @@ protected:
     }
     virtual captureTuple popForAtomicForwardCapture(RegexMatcher<USE_STRINGS> &matcher, Uint captureNum)
     {
-        __assume(0);
+        UNREACHABLE_CODE;
     }
     virtual bool okayToTryAlternatives(RegexMatcher<USE_STRINGS> &matcher)
     {
@@ -862,12 +862,12 @@ class Backtrack_LeaveGroupLazily : public Backtrack_LeaveGroup<USE_STRINGS>
     {
         Backtrack_LeaveGroup<USE_STRINGS>::popTo(matcher);
 
-        if (matcher.groupStackTop->loopCount == MAX_EXTEND(group->maxCount) ||
+        if (matcher.groupStackTop->loopCount == MAX_EXTEND(this->group->maxCount) ||
             positionDiff == 0 && matcher.groupStackTop->group->maxCount == UINT_MAX && matcher.groupStackTop->loopCount >= matcher.groupStackTop->group->minCount)
         {
             matcher.position = matcher.groupStackTop->position -= positionDiff;
             matcher.alternative = &nullAlternative;
-            popCapture(matcher);
+            this->popCapture(matcher);
             return false;
         }
 
@@ -878,7 +878,7 @@ class Backtrack_LeaveGroupLazily : public Backtrack_LeaveGroup<USE_STRINGS>
     virtual void fprintDebug(RegexMatcher<USE_STRINGS> &matcher, FILE *f)
     {
         fputs("Backtrack_LeaveGroupLazily", f);
-        Backtrack_LeaveGroup::fprintDebugBase(matcher, f);
+        Backtrack_LeaveGroup<USE_STRINGS>::fprintDebugBase(matcher, f);
         fprintf(f, ", positionDiff=%llu\n", positionDiff);
     }
 };

regex.h
@@ -94,6 +94,8 @@ class RegexSymbol
     friend class Backtrack_SkipGroup<true>;
     friend class Backtrack_EnterGroup<false>;
     friend class Backtrack_EnterGroup<true>;
+    friend class Backtrack_LeaveGroupLazily<false>;
+    friend class Backtrack_LeaveGroupLazily<true>;
     friend class Backtrack_LoopGroup<false>;
     friend class Backtrack_LoopGroup<true>;
     friend class Backtrack_TryMatch<false>;

tools.h
@@ -38,6 +38,20 @@ template <size_t size>
 char (*__strlength_helper(char const (&_String)[size]))[size];
 #define strlength(_String) (sizeof(*__strlength_helper(_String))-1)
 
+#ifdef _MSC_VER
+#define UNREACHABLE_CODE __assume(0)
+#else
+#define UNREACHABLE_CODE
+#endif
+
+#ifdef _MSC_VER
+#define ALWAYS_INLINE __forceinline
+#elif __has_attribute(always_inline)
+#define ALWAYS_INLINE __attribute__((always_inline))
+#else
+#define ALWAYS_INLINE
+#endif
+
 template <typename UINT_TYPE>
 UINT_TYPE readNumericConstant(const char *&buf);
 