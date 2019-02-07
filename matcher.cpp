Davidebyzero/RegexMathEngine

commit 7047d9edd5e3362f01019a132722906c392ce9d9

Fix ECMAScript compliance when dealing with empty matches in a looped group (issue #1)
The previous behavior was PCRE compliant, and is still used when "--pcre" or "--neam-" are specified.

matcher.cpp
@@ -1241,6 +1241,13 @@ bool RegexMatcher<USE_STRINGS>::Match(RegexGroup &regex, Uint numCaptureGroups, 
                 if (group->lazy && groupStackTop->loopCount >= group->minCount)
                     leaveLazyGroup();
                 else
+                if (no_empty_after_minimum && position == groupStackTop->position && group->minCount != group->maxCount && inrange(groupStackTop->loopCount, group->minCount+1, group->maxCount))
+                {
+                    if (debugTrace)
+                        fputs("Empty match found in group with maximum quantifer unsatisfied; treating this as a non-match\n\n", stderr);
+                    nonMatch();
+                }
+                else
                 if (groupStackTop->loopCount == MAX_EXTEND(group->maxCount) || group->maxCount == UINT_MAX && groupStackTop->loopCount >= group->minCount && position == groupStackTop->position)
                     leaveMaxedOutGroup();
                 else

regex.cpp
@@ -76,6 +76,7 @@ Uint debugTrace = 0;
 bool free_spacing_mode = false;
 bool emulate_ECMA_NPCGs = true;
 bool allow_empty_character_classes = true;
+bool no_empty_after_minimum = true;
 bool allow_quantifiers_on_assertions = true;
 bool allow_molecular_lookahead = false;
 bool allow_atomic_groups = false;
@@ -118,6 +119,13 @@ Options:\n\
                       or \"[^]\", is permitted. \"-\" makes it an error to attempt\n\
                       to use them (as in most regex engines), and \"+\" allows\n\
                       them (as in ECMAScript). The default is \"+\".\n\
+  --neam{-|+}         Specifies what to do when an empty match occurs in a group\n\
+                      that has a minimum and maximum quantifier different from\n\
+                      each other (e.g. \"?\", \"+\", \"{3,5}\"). \"-\" makes the engine\n\
+                      exit the group and not attempt to fulfill the maximum.\n\
+                      \"+\" makes it backtrack in an attempt to find a non-empty\n\
+                      match. The default is \"+\" (standard ECMAScript behavior).\n\
+                      Stands for \"no empty after minimum\".\n\
   --qa{-|+}           Specifies whether to allow quantifiers on assertions:\n\
                       lookaheads, anchors, and word boundaries/nonboundaries.\n\
                       \"-\" disallows them, and \"+\" allows them (the default).\n\
@@ -137,7 +145,8 @@ Options:\n\
                                   (*PRUNE), (*SKIP), and (*THEN)\n\
                       all  Enable all of the above extensions\n\
   --pcre              Emulate PCRE as closely as currently possible. This\n\
-                      is equivalent to \"-x ag,pq,cnd,rs,pbr --npcg- --ecc-\".\n\
+                      is equivalent to:\n\
+                      --npcg- --ecc- --neam- -x ag,pq,cnd,rs,pbr\n\
   -o                  Show only the part of the line that matched\n\
   -O NUMBER           Specifies the optimization level, from 0 to 2. This\n\
                       controls whether optimizations are enabled which skip\n\
@@ -289,6 +298,14 @@ int main(int argc, char *argv[])
                     allow_empty_character_classes = argv[i][2 + strlength("ecc")] == '+';
                 }
                 else
+                if (strncmp(&argv[i][2], "neam", strlength("neam"))==0 &&
+                    (argv[i][2 + strlength("neam")] == '-' ||
+                     argv[i][2 + strlength("neam")] == '+' ) &&
+                    !argv[i][2 + strlength("neam") + 1])
+                {
+                    no_empty_after_minimum = argv[i][2 + strlength("neam")] == '+';
+                }
+                else
                 if (strncmp(&argv[i][2], "qa", strlength("qa"))==0 &&
                     (argv[i][2 + strlength("qa")] == '-' ||
                      argv[i][2 + strlength("qa")] == '+' ) &&
@@ -301,6 +318,7 @@ int main(int argc, char *argv[])
                 {
                     emulate_ECMA_NPCGs = false;
                     allow_empty_character_classes = false;
+                    no_empty_after_minimum = false;
                     allow_quantifiers_on_assertions = true;
                     allow_molecular_lookahead = false;
                     allow_atomic_groups = true;

regex.h
@@ -23,6 +23,7 @@ extern Uint debugTrace;
 extern bool free_spacing_mode;
 extern bool emulate_ECMA_NPCGs;
 extern bool allow_empty_character_classes;
+extern bool no_empty_after_minimum;
 extern bool allow_quantifiers_on_assertions;
 extern bool allow_molecular_lookahead;
 extern bool allow_atomic_groups;

commit 62be5662a50f67eec43e2404033c5f855fcf1121

debugTrace: Fix printout when NEAM+ causes a non-match
Bug introduced in 7047d9edd5e3362f01019a132722906c392ce9d9

matcher.cpp
@@ -17,14 +17,24 @@ RegexPattern *nullAlternative = NULL;
 RegexSymbol  *nullSymbol      = NULL;
 
 template <bool USE_STRINGS>
-void RegexMatcher<USE_STRINGS>::nonMatch(bool negativeLookahead)
+void RegexMatcher<USE_STRINGS>::nonMatch(NonMatchType type)
 {
     if (debugTrace)
     {
-        if (negativeLookahead)
-            fputs("Match found inside negative lookahead, resulting in a non-match outside it\n\n", stderr);
-        else
+        switch (type)
+        {
+        case NonMatch_Default:
             fputs(": non-match", stderr);
+            break;
+        case NonMatch_NegativeLookahead:
+            fputs("Match found inside negative lookahead, resulting in a non-match outside it\n\n", stderr);
+            break;
+        case NonMatch_NEAM:
+            fputs("Empty match found in group with maximum quantifer unsatisfied; treating this as a non-match\n\n", stderr);
+            break;
+        default:
+            UNREACHABLE_CODE;
+        }
     }
 
     position = groupStackTop->position;
@@ -1219,7 +1229,7 @@ bool RegexMatcher<USE_STRINGS>::Match(RegexGroup &regex, Uint numCaptureGroups, 
                         symbol = *alternative ? (*alternative)->symbols : &nullSymbol;
                     }
                     else
-                        nonMatch(true);
+                        nonMatch(NonMatch_NegativeLookahead);
                     continue;
                 }
 
@@ -1231,11 +1241,7 @@ bool RegexMatcher<USE_STRINGS>::Match(RegexGroup &regex, Uint numCaptureGroups, 
                     leaveLazyGroup();
                 else
                 if (no_empty_after_minimum && position == groupStackTop->position && group->minCount != group->maxCount && inrange(groupStackTop->loopCount, group->minCount+1, group->maxCount))
-                {
-                    if (debugTrace)
-                        fputs("Empty match found in group with maximum quantifer unsatisfied; treating this as a non-match\n\n", stderr);
-                    nonMatch();
-                }
+                    nonMatch(NonMatch_NEAM);
                 else
                 if (groupStackTop->loopCount == MAX_EXTEND(group->maxCount) || group->maxCount == UINT_MAX && groupStackTop->loopCount >= group->minCount && position == groupStackTop->position)
                     leaveMaxedOutGroup();

matcher.h
@@ -173,7 +173,13 @@ class RegexMatcher : public RegexMatcherBase<USE_STRINGS>
     char match; // zero = looking for match, negative = match failed, positive = match found
     bool anchored; // indicates whether we can optimize the search by only trying a match at the start
 
-    void nonMatch(bool negativeLookahead = false);
+    enum NonMatchType
+    {
+        NonMatch_Default,
+        NonMatch_NegativeLookahead,
+        NonMatch_NEAM,
+    };
+    void nonMatch(NonMatchType type = NonMatch_Default);
     void yesMatch(Uint64 newPosition, bool haveChoice);
     void pushStack();
     void enterGroup(RegexGroup *group);