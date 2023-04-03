Davidebyzero/RegexMathEngine

commit 8aeb9e023ee2eaff8fc690f2b4d8cae4121699ee

Fix implementation of molecular lookinto
The input extent and position were not properly popped when exiting a molecular lookinto.

matcher.cpp
@@ -341,13 +341,16 @@ void *RegexMatcher<USE_STRINGS>::loopGroup(Backtrack_LoopGroup<USE_STRINGS> *pus
 
 bool matchWordCharacter(Uchar ch);
 
-template<> void RegexMatcher<false>::initInput(Uint64 _input, Uint numCaptureGroups)
+template<> void RegexMatcher<false>::initInput(Uint64 _input, Uint numCaptureGroups, Uint maxLookintoDepth)
 {
     input = input0 = _input;
     basicCharIsWordCharacter = matchWordCharacter(basicChar);
 }
-template<> void RegexMatcher<true>::initInput(Uint64 _input, Uint numCaptureGroups)
+template<> void RegexMatcher<true>::initInput(Uint64 _input, Uint numCaptureGroups, Uint maxLookintoDepth)
 {
+    stringLookintoBase = new const char * [maxLookintoDepth];
+    stringLookintoTop = stringLookintoBase;
+
     stringToMatchAgainst = stringToMatchAgainst0 = (const char *)_input;
     input = input0 = strlen(stringToMatchAgainst);
     delete [] captureOffsets;
@@ -1112,12 +1115,37 @@ void RegexMatcher<USE_STRINGS>::popAtomicGroup(RegexGroup *const group)
     }
 }
 
+template<> void RegexMatcher<false>::pushLookintoInput(Uint64 newInput, const char *newStringToMatchAgainst)
+{
+    *inputLookintoTop++  = input;
+    input = newInput;
+}
+template<> void RegexMatcher<true>::pushLookintoInput(Uint64 newInput, const char *newStringToMatchAgainst)
+{
+    *inputLookintoTop++  = input;
+    *stringLookintoTop++ = stringToMatchAgainst;
+    input = newInput;
+    stringToMatchAgainst = newStringToMatchAgainst ? newStringToMatchAgainst : stringToMatchAgainst0;
+}
+
+template<> void RegexMatcher<false>::popLookintoInput()
+{
+    input = *--inputLookintoTop;
+}
+template<> void RegexMatcher<true>::popLookintoInput()
+{
+    input = *--inputLookintoTop;
+    stringToMatchAgainst = *--stringLookintoTop;
+}
+
 template <bool USE_STRINGS>
-bool RegexMatcher<USE_STRINGS>::Match(RegexGroupRoot &regex, Uint numCaptureGroups, Uint maxGroupDepth, Uint64 _input, Uint returnMatch_backrefIndex, Uint64 &returnMatchOffset, Uint64 &returnMatchLength, Uint64 *possibleMatchesCount_ptr)
+bool RegexMatcher<USE_STRINGS>::Match(RegexGroupRoot &regex, Uint numCaptureGroups, Uint maxGroupDepth, Uint maxLookintoDepth, Uint64 _input, Uint returnMatch_backrefIndex, Uint64 &returnMatchOffset, Uint64 &returnMatchLength, Uint64 *possibleMatchesCount_ptr)
 {
     delete [] groupStackBase;
     groupStackBase = new GroupStackNode [maxGroupDepth];
     groupStackTop = groupStackBase;
+    inputLookintoBase = new Uint64 [maxLookintoDepth];
+    inputLookintoTop = inputLookintoBase;
     if (matchFunction(&regex) != &RegexMatcher<USE_STRINGS>::matchSymbol_Group)
         virtualizeSymbols(&regex);
 
@@ -1141,7 +1169,7 @@ bool RegexMatcher<USE_STRINGS>::Match(RegexGroupRoot &regex, Uint numCaptureGrou
 
     verb = RegexVerb_None;
 
-    initInput(_input, numCaptureGroups);
+    initInput(_input, numCaptureGroups, maxLookintoDepth);
 
     if (possibleMatchesCount_ptr)
         *possibleMatchesCount_ptr = 0;
@@ -1249,6 +1277,9 @@ bool RegexMatcher<USE_STRINGS>::Match(RegexGroupRoot &regex, Uint numCaptureGrou
                     pushStack->numCaptured = groupStackTop->numCaptured;
                     pushStack->alternative = (Uint)(alternative - groupStackTop->group->alternatives);
 
+                    if (group->type == RegexGroup_LookintoMolecular)
+                        popLookintoInput();
+
                     position    = groupStackTop->position;
                     alternative = group->parentAlternative;
                     if (!group->self) // group->self will be NULL if this is the lookaround in a conditional
@@ -1336,8 +1367,12 @@ bool RegexMatcher<USE_STRINGS>::Match(RegexGroupRoot &regex, Uint numCaptureGrou
                 delete [] copy;
 
                 if (debugTrace > 1)
+                {
                     for (GroupStackNode *i = groupStackTop; i >= groupStackBase; i--)
                         fprintf(stderr, "  %llu, #%llu, (%u): %s\n", i->position, i->loopCount, i->numCaptured, i->group->originalCode);
+                    for (Uint64 *i = inputLookintoTop; --i >= inputLookintoBase;)
+                        fprintf(stderr, "  (?^{%llu})\n", *i);
+                }
 #ifdef _DEBUG
                 fprintf(stderr, "Step %llu: {%llu|%llu} <%llu> ", numSteps, position, input - position, stack.getStackDepth());
 #else
@@ -1564,5 +1599,5 @@ void RegexMatcher<true>::fprintCapture(FILE *f, Uint i)
     fprintCapture(f, captures[i], captureOffsets[i]);
 }
 
-template bool RegexMatcher<false>::Match(RegexGroupRoot &regex, Uint numCaptureGroups, Uint maxGroupDepth, Uint64 _input, Uint returnMatch_backrefIndex, Uint64 &returnMatchOffset, Uint64 &returnMatchLength, Uint64 *possibleMatchesCount_ptr);
-template bool RegexMatcher<true >::Match(RegexGroupRoot &regex, Uint numCaptureGroups, Uint maxGroupDepth, Uint64 _input, Uint returnMatch_backrefIndex, Uint64 &returnMatchOffset, Uint64 &returnMatchLength, Uint64 *possibleMatchesCount_ptr);
+template bool RegexMatcher<false>::Match(RegexGroupRoot &regex, Uint numCaptureGroups, Uint maxGroupDepth, Uint maxLookintoDepth, Uint64 _input, Uint returnMatch_backrefIndex, Uint64 &returnMatchOffset, Uint64 &returnMatchLength, Uint64 *possibleMatchesCount_ptr);
+template bool RegexMatcher<true >::Match(RegexGroupRoot &regex, Uint numCaptureGroups, Uint maxGroupDepth, Uint maxLookintoDepth, Uint64 _input, Uint returnMatch_backrefIndex, Uint64 &returnMatchOffset, Uint64 &returnMatchLength, Uint64 *possibleMatchesCount_ptr);

matcher.h
@@ -124,6 +124,9 @@ struct RegexMatcherBase<true>
     const char *stringToMatchAgainst;
     const char **captureOffsets;
     const char **captureOffsetsAtomicTmp; // only used with enable_persistent_backrefs
+
+    const char **stringLookintoBase;
+    const char **stringLookintoTop;
 };
 
 template <bool USE_STRINGS>
@@ -162,6 +165,9 @@ class RegexMatcher : public RegexMatcherBase<USE_STRINGS>
     Uint *captureIndexesAtomicTmp; // only used with enable_persistent_backrefs
     Uint64 *capturesAtomicTmp; // only used with enable_persistent_backrefs
 
+    Uint64 *inputLookintoBase;
+    Uint64 *inputLookintoTop;
+
     RegexVerb verb; // can only be RegexVerb_None, RegexVerb_Commit, RegexVerb_Prune, RegexVerb_Skip, or RegexVerb_Then
     Uint64 skipPosition; // for RegexVerb_Skip
     Backtrack<USE_STRINGS> stack;
@@ -198,8 +204,10 @@ class RegexMatcher : public RegexMatcherBase<USE_STRINGS>
     Backtrack_LoopGroup<USE_STRINGS> *pushStack_LoopGroup();
     void *loopGroup(Backtrack_LoopGroup<USE_STRINGS> *pushLoop, Uint64 pushPosition, Uint64 oldPosition, Uint alternativeNum);
     void popAtomicGroup(RegexGroup *const group);
+    inline void pushLookintoInput(Uint64 newInput, const char *newStringToMatchAgainst);
+    inline void  popLookintoInput();
 
-    inline void initInput(Uint64 _input, Uint numCaptureGroups);
+    inline void initInput(Uint64 _input, Uint numCaptureGroups, Uint maxLookintoDepth);
     inline void  readCapture(Uint index, Uint64 &multiple, const char *&pBackref);
     inline void writeCapture(Uint index, Uint64  multiple, const char * pBackref);
     inline bool writeCaptureRelative(Uint index, Uint64 start, Uint64 end); // returns true iff this changes the capture's value
@@ -270,8 +278,13 @@ class RegexMatcher : public RegexMatcherBase<USE_STRINGS>
 public:
     inline RegexMatcher();
     inline ~RegexMatcher();
-    bool Match(RegexGroupRoot &regex, Uint numCaptureGroups, Uint maxGroupDepth, Uint64 _input, Uint returnMatch_backrefIndex, Uint64 &returnMatchOffset, Uint64 &returnMatchLength, Uint64 *possibleMatchesCount_ptr);
+    bool Match(RegexGroupRoot &regex, Uint numCaptureGroups, Uint maxGroupDepth, Uint maxLookintoDepth, Uint64 _input, Uint returnMatch_backrefIndex, Uint64 &returnMatchOffset, Uint64 &returnMatchLength, Uint64 *possibleMatchesCount_ptr);
 };
+
+template <> void RegexMatcher<false>::pushLookintoInput(Uint64 newInput, const char *newStringToMatchAgainst);
+template <> void RegexMatcher<false>::popLookintoInput();
+template <> void RegexMatcher<true>::pushLookintoInput(Uint64 newInput, const char *newStringToMatchAgainst);
+template <> void RegexMatcher<true>::popLookintoInput();
 
 template <> void RegexMatcher<false>::fprintCapture(FILE *f, Uint64 length, const char *offset);
 template <> void RegexMatcher<false>::fprintCapture(FILE *f, Uint i);
@@ -803,12 +816,10 @@ template <bool USE_STRINGS> class Backtrack_EnterGroupLookinto;
 template <> class Backtrack_EnterGroupLookinto<false> : public Backtrack_EnterGroup<false>
 {
     friend class RegexMatcher<false>;
-    Uint64 inputOutside;
 
     void pushInput(RegexMatcher<false> &matcher, Uint64 newInput, const char *newStringToMatchAgainst)
     {
-        inputOutside = matcher.input;
-        matcher.input = newInput;
+        matcher.pushLookintoInput(newInput, newStringToMatchAgainst);
     }
     virtual size_t getSize(RegexMatcher<false> &matcher)
     {
@@ -816,17 +827,17 @@ template <> class Backtrack_EnterGroupLookinto<false> : public Backtrack_EnterGr
     }
     virtual bool popTo(RegexMatcher<false> &matcher)
     {
-        matcher.input = inputOutside;
+        matcher.popLookintoInput();
         return Backtrack_EnterGroup<false>::popTo(matcher);
     }
     virtual void popForNegativeLookahead(RegexMatcher<false> &matcher)
     {
-        matcher.input = inputOutside;
+        matcher.popLookintoInput();
         return Backtrack_EnterGroup<false>::popForNegativeLookahead(matcher);
     }
     virtual int popForAtomicCapture(RegexMatcher<false> &matcher)
     {
-        matcher.input = inputOutside;
+        matcher.popLookintoInput();
         return Backtrack_EnterGroup<false>::popForAtomicCapture(matcher);
     }
     virtual void fprintDebug(RegexMatcher<false> &matcher, FILE *f)
@@ -837,15 +848,10 @@ template <> class Backtrack_EnterGroupLookinto<false> : public Backtrack_EnterGr
 template <> class Backtrack_EnterGroupLookinto<true> : public Backtrack_EnterGroup<true>
 {
     friend class RegexMatcher<true>;
-    Uint64 inputOutside;
-    const char *stringToMatchAgainstOutside;
 
     void pushInput(RegexMatcher<true> &matcher, Uint64 newInput, const char *newStringToMatchAgainst)
     {
-        inputOutside = matcher.input;
-        stringToMatchAgainstOutside = matcher.stringToMatchAgainst;
-        matcher.input = newInput;
-        matcher.stringToMatchAgainst = newStringToMatchAgainst ? newStringToMatchAgainst : matcher.stringToMatchAgainst0;
+        matcher.pushLookintoInput(newInput, newStringToMatchAgainst);
     }
     virtual size_t getSize(RegexMatcher<true> &matcher)
     {
@@ -853,20 +859,17 @@ template <> class Backtrack_EnterGroupLookinto<true> : public Backtrack_EnterGro
     }
     virtual bool popTo(RegexMatcher<true> &matcher)
     {
-        matcher.input = inputOutside;
-        matcher.stringToMatchAgainst = stringToMatchAgainstOutside;
+        matcher.popLookintoInput();
         return Backtrack_EnterGroup<true>::popTo(matcher);
     }
     virtual void popForNegativeLookahead(RegexMatcher<true> &matcher)
     {
-        matcher.input = inputOutside;
-        matcher.stringToMatchAgainst = stringToMatchAgainstOutside;
+        matcher.popLookintoInput();
         return Backtrack_EnterGroup<true>::popForNegativeLookahead(matcher);
     }
     virtual int popForAtomicCapture(RegexMatcher<true> &matcher)
     {
-        matcher.input = inputOutside;
-        matcher.stringToMatchAgainst = stringToMatchAgainstOutside;
+        matcher.popLookintoInput();
         return Backtrack_EnterGroup<true>::popForAtomicCapture(matcher);
     }
     virtual void fprintDebug(RegexMatcher<true> &matcher, FILE *f)
@@ -932,6 +935,13 @@ class Backtrack_LeaveMolecularLookahead : public BacktrackNode<USE_STRINGS>
         matcher.groupStackTop->numCaptured = numCaptured;
         matcher.groupStackTop[-1].numCaptured -= numCaptured;
         matcher.alternative = group->alternatives + alternative;
+
+        Uint64      inputLookintoSize;
+        const char *inputLookintoPtr = NULL;
+        if (!matcher.getLookintoEntrace(((RegexGroupLookinto*)group)->backrefIndex, inputLookintoSize, inputLookintoPtr))
+            return false;
+        matcher.pushLookintoInput(inputLookintoSize, inputLookintoPtr);
+
         return false;
     }
     virtual void popForNegativeLookahead(RegexMatcher<USE_STRINGS> &matcher)

parser.cpp
@@ -214,6 +214,8 @@ RegexParser::RegexParser(RegexGroupRoot &regex, const char *buf)
     backrefIndex = 0;
     maxGroupDepth = 1;
     curGroupDepth = 1;
+    maxLookintoDepth = 0;
+    curLookintoDepth = 0;
 
     for (;;)
     {
@@ -528,6 +530,16 @@ RegexParser::RegexParser(RegexGroupRoot &regex, const char *buf)
                     curGroupDepth++;
                 if (maxGroupDepth < curGroupDepth)
                     maxGroupDepth = curGroupDepth;
+                switch (group->type)
+                {
+                case RegexGroup_Lookinto:
+                case RegexGroup_LookintoMolecular:
+                case RegexGroup_NegativeLookinto:
+                    curLookintoDepth++;
+                    if (maxLookintoDepth < curLookintoDepth)
+                        maxLookintoDepth = curLookintoDepth;
+                    break;
+                }
 
                 group->parentAlternative = NULL;
                 group->self = NULL;
@@ -584,7 +596,17 @@ RegexParser::RegexParser(RegexGroupRoot &regex, const char *buf)
                 curGroupDepth--;
                 // Neither the current lookaround nor atomic group matching code can handle quantifiers, so leave room in case we will be wrapping it in a non-capturing group (can't know at this point if it has a quantifier or not)
                 if (group->isLookaround() || group->type == RegexGroup_Atomic)
+                {
                     curGroupDepth--;
+                    switch (group->type)
+                    {
+                    case RegexGroup_Lookinto:
+                    case RegexGroup_LookintoMolecular:
+                    case RegexGroup_NegativeLookinto:
+                        curLookintoDepth--;
+                        break;
+                    }
+                }
 
                 ParsingStack *stackDown = stack->below;
                 delete stack;

parser.h
@@ -35,6 +35,8 @@ class RegexParser
     Uint backrefIndex; // zero-numbered; 0 corresponds to \1
     Uint maxGroupDepth; // minimum is 1 (meaning, root group only)
     Uint curGroupDepth;
+    Uint maxLookintoDepth;
+    Uint curLookintoDepth;
     bool symbolCountSpecified;
     bool symbolLazinessSpecified;
 

regex.cpp
@@ -21,6 +21,7 @@ class Regex
     RegexGroupRoot regex;
     Uint numCaptureGroups;
     Uint maxGroupDepth;
+    Uint maxLookintoDepth;
 public:
     Regex(const char *buf);
     bool MatchNumber(Uint64 input, char basicChar, Uint returnMatch_backrefIndex, Uint64 &returnMatch, Uint64 *possibleMatchesCount_ptr);
@@ -38,6 +39,7 @@ Regex::Regex(const char *buf)
     RegexParser parser(regex, buf);
     numCaptureGroups = parser.backrefIndex;
     maxGroupDepth    = parser.maxGroupDepth;
+    maxLookintoDepth = parser.maxLookintoDepth;
 }
 
 bool Regex::MatchNumber(Uint64 input, char basicChar, Uint returnMatch_backrefIndex, Uint64 &returnMatch, Uint64 *possibleMatchesCount_ptr=NULL)
@@ -45,13 +47,13 @@ bool Regex::MatchNumber(Uint64 input, char basicChar, Uint returnMatch_backrefIn
     RegexMatcher<false> match;
     match.basicChar = basicChar;
     Uint64 returnMatchOffset;
-    return match.Match(regex, numCaptureGroups, maxGroupDepth, input, returnMatch_backrefIndex, returnMatchOffset, returnMatch, possibleMatchesCount_ptr);
+    return match.Match(regex, numCaptureGroups, maxGroupDepth, maxLookintoDepth, input, returnMatch_backrefIndex, returnMatchOffset, returnMatch, possibleMatchesCount_ptr);
 }
 
 bool Regex::MatchString(const char *stringToMatchAgainst, Uint returnMatch_backrefIndex, const char *&returnMatch, size_t &returnMatchLength, Uint64 *possibleMatchesCount_ptr=NULL)
 {
     RegexMatcher<true> match;
-    bool result = match.Match(regex, numCaptureGroups, maxGroupDepth, (Uint64)stringToMatchAgainst, returnMatch_backrefIndex, (Uint64 &)returnMatch, (Uint64 &)returnMatchLength, possibleMatchesCount_ptr);
+    bool result = match.Match(regex, numCaptureGroups, maxGroupDepth, maxLookintoDepth, (Uint64)stringToMatchAgainst, returnMatch_backrefIndex, (Uint64 &)returnMatch, (Uint64 &)returnMatchLength, possibleMatchesCount_ptr);
     (const char *&)returnMatch = stringToMatchAgainst + (size_t)(Uint64 &)returnMatch;
     return result;
 }

regex.h
@@ -267,6 +267,8 @@ class RegexGroupLookinto : public RegexGroup
     friend class RegexParser;
     friend class RegexMatcher<false>;
     friend class RegexMatcher<true>;
+    friend class Backtrack_LeaveMolecularLookahead<false>;
+    friend class Backtrack_LeaveMolecularLookahead<true>;
     Uint backrefIndex; // UINT_MAX = input string; 0 = the match so far; 1... = \1...
 public:
     RegexGroupLookinto(RegexGroupType type, Uint backrefIndex) : RegexGroup(type), backrefIndex(backrefIndex) {}

commit 167c351d144b315ceb1117aabe755a11c53c6314

Fix memory leak introduced in amended implementation of molecular lookinto
Bug introduced in 8aeb9e023ee2eaff8fc690f2b4d8cae4121699ee

matcher.cpp
@@ -346,6 +346,7 @@ template<> void RegexMatcher<false>::initInput(Uint64 _input, Uint numCaptureGro
 }
 template<> void RegexMatcher<true>::initInput(Uint64 _input, Uint numCaptureGroups, Uint maxLookintoDepth)
 {
+    delete [] stringLookintoBase;
     stringLookintoBase = new const char * [maxLookintoDepth];
     stringLookintoTop = stringLookintoBase;
 
@@ -1145,6 +1146,7 @@ bool RegexMatcher<USE_STRINGS>::Match(RegexGroupRoot &regex, Uint numCaptureGrou
     delete [] groupStackBase;
     groupStackBase = new GroupStackNode [maxGroupDepth];
     groupStackTop = groupStackBase;
+    delete [] inputLookintoBase;
     inputLookintoBase = new Uint64 [maxLookintoDepth];
     inputLookintoTop = inputLookintoBase;
     if (matchFunction(&regex) != &RegexMatcher<USE_STRINGS>::matchSymbol_Group)

matcher.h
@@ -1637,7 +1637,8 @@ class Backtrack_ResetStart : public BacktrackNode<USE_STRINGS>
 
 template<> inline RegexMatcher<false>::RegexMatcher() :
     groupStackBase(NULL),
-    captureStackBase(NULL)
+    captureStackBase(NULL),
+    inputLookintoBase(NULL)
 #ifdef _DEBUG
     ,matcher(*this)
 #endif
@@ -1652,7 +1653,8 @@ template<> inline RegexMatcher<false>::RegexMatcher() :
 }
 template<> inline RegexMatcher<true>::RegexMatcher() :
     groupStackBase(NULL),
-    captureStackBase(NULL)
+    captureStackBase(NULL),
+    inputLookintoBase(NULL)
 #ifdef _DEBUG
     ,matcher(*this)
 #endif
@@ -1666,11 +1668,13 @@ template<> inline RegexMatcher<true>::RegexMatcher() :
         capturesAtomicTmp = NULL;
         captureOffsetsAtomicTmp = NULL;
     }
+    stringLookintoBase = NULL;
 }
 
 template<> inline RegexMatcher<false>::~RegexMatcher()
 {
     delete [] groupStackBase;
+    delete [] inputLookintoBase;
     delete [] captureStackBase;
     delete [] captures;
     if (enable_persistent_backrefs)
@@ -1683,6 +1687,7 @@ template<> inline RegexMatcher<false>::~RegexMatcher()
 template<> inline RegexMatcher<true>::~RegexMatcher()
 {
     delete [] groupStackBase;
+    delete [] inputLookintoBase;
     delete [] captureStackBase;
     delete [] captures;
     delete [] captureOffsets;
@@ -1693,6 +1698,7 @@ template<> inline RegexMatcher<true>::~RegexMatcher()
         delete [] capturesAtomicTmp;
         delete [] captureOffsetsAtomicTmp;
     }
+    delete [] stringLookintoBase;
 }
 
 #pragma pack( pop )