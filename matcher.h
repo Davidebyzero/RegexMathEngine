Davidebyzero/RegexMathEngine

commit 8b7cdbdfb811504128d9dafbe1748f43d5bce06b

Fix implementation of constant-group optimization
* For capturing groups, it was leaving the group unset when a zero-length group was repeated a nonzero number of times (i.e., the cc34ccfb316434368f10cac1723038b65e50e03a was not actually foolproof)
* It was not pushing the previous value of the capture in enable_persistent_backrefs mode
* [debug] Backtrack_LeaveConstGroupCapturing::fprintDebug() was not printing its corresponding backref number in a good way

Feature initially implemented in 0ecc68e9745919571f536e79ac2c05d15ec38e28

matcher.h
@@ -184,7 +184,6 @@ class RegexMatcher : public RegexMatcherBase<USE_STRINGS>
     Uint64 numSteps;
 
     char match; // zero = looking for match, negative = match failed, positive = match found
-    bool nonMatchHappened;
 
     enum NonMatchType
     {
@@ -258,12 +257,12 @@ class RegexMatcher : public RegexMatcherBase<USE_STRINGS>
     void matchSymbol_IsPrime                 (RegexSymbol *thisSymbol);
     void matchSymbol_IsPowerOf2              (RegexSymbol *thisSymbol);
 
-    Uint64 matchSymbol_ConstGroup(RegexSymbol *thisSymbol);
+    Uint64 matchSymbol_ConstGroup(RegexSymbol *thisSymbol, bool capturing);
 
     template <typename MATCH_TYPE>
-    inline bool runtimeOptimize_matchSymbol_Character_or_Backref(RegexSymbol *const thisSymbol, Uint64 const multiple, MATCH_TYPE const repetend);
+    inline int8 runtimeOptimize_matchSymbol_Character_or_Backref(RegexSymbol *const thisSymbol, Uint64 const multiple, MATCH_TYPE const repetend);
     template <typename MATCH_TYPE>
-    void matchSymbol_Character_or_Backref (RegexSymbol *thisSymbol, Uint64 multiple, MATCH_TYPE pBackref);
+    bool matchSymbol_Character_or_Backref (RegexSymbol *thisSymbol, Uint64 multiple, MATCH_TYPE pBackref);
 
     inline void (RegexMatcher<USE_STRINGS>::*&matchFunction(RegexSymbol *thisSymbol))(RegexSymbol *thisSymbol);
     inline bool characterCanMatch(RegexSymbol *thisSymbol);
@@ -974,6 +973,8 @@ class Backtrack_LeaveConstGroupCapturing : public BacktrackNode<USE_STRINGS>
 {
     friend class RegexMatcher<USE_STRINGS>;
     Uint backrefIndex;
+    Uint8 buffer[FLEXIBLE_SIZE_ARRAY];
+
     void popCaptureGroup(RegexMatcher<USE_STRINGS> &matcher)
     {
         matcher.captures[backrefIndex] = NON_PARTICIPATING_CAPTURE_GROUP;
@@ -985,13 +986,33 @@ class Backtrack_LeaveConstGroupCapturing : public BacktrackNode<USE_STRINGS>
 #endif
     }
 protected:
-    virtual void popCapture(RegexMatcher<USE_STRINGS> &matcher)
+    void popCapture(RegexMatcher<USE_STRINGS> &matcher)
     {
+        const char *&dummy = (const char *&)buffer;
+        if (!USE_STRINGS)
+            matcher.writeCapture(backrefIndex, *(Uint64*)(buffer                      ), dummy);
+        else
+            matcher.writeCapture(backrefIndex, *(Uint64*)(buffer + sizeof(const char*)), *(const char**)buffer);
+        if (matcher.captures[backrefIndex] == NON_PARTICIPATING_CAPTURE_GROUP)
+        {
+            matcher.captureStackTop--;
+            matcher.groupStackTop->numCaptured--;
+#ifdef _DEBUG
+            if (*matcher.captureStackTop != backrefIndex)
+                THROW_ENGINEBUG;
+#endif
+        }
     }
 
+    static size_t get_size()
+    {
+        return (size_t)&((Backtrack_LeaveConstGroupCapturing*)0)->buffer +
+            (enable_persistent_backrefs ? (sizeof(Uint64) + (USE_STRINGS ? sizeof(const char*) : 0))
+                                        : 0);
+    }
     virtual size_t getSize(RegexMatcher<USE_STRINGS> &matcher)
     {
-        return sizeof(*this);
+        return get_size();
     }
     virtual bool popTo(RegexMatcher<USE_STRINGS> &matcher)
     {
@@ -1007,11 +1028,16 @@ protected:
     }
     virtual int popForAtomicCapture(RegexMatcher<USE_STRINGS> &matcher)
     {
-        return 1;
+        return -1;
     }
     virtual captureTuple popForAtomicForwardCapture(RegexMatcher<USE_STRINGS> &matcher, Uint captureNum)
     {
-        return captureTuple(matcher.captures[backrefIndex], NULL, backrefIndex);
+        if (!enable_persistent_backrefs)
+            return captureTuple(NON_PARTICIPATING_CAPTURE_GROUP         , NULL                 , backrefIndex);
+        if (!USE_STRINGS)
+            return captureTuple(*(Uint64*)(buffer                      ), NULL                 , backrefIndex);
+        else
+            return captureTuple(*(Uint64*)(buffer + sizeof(const char*)), *(const char**)buffer, backrefIndex);
     }
     virtual bool okayToTryAlternatives(RegexMatcher<USE_STRINGS> &matcher)
     {
@@ -1019,7 +1045,15 @@ protected:
     }
     virtual void fprintDebugBase(RegexMatcher<USE_STRINGS> &matcher, FILE *f)
     {
-        fprintf(f, ": backrefIndex=%u", backrefIndex);
+        fprintf(f, ": backref=\\%u", backrefIndex+1);
+        if (enable_persistent_backrefs)
+        {
+            fputs(", capture=", f);
+            if (!USE_STRINGS)
+                matcher.fprintCapture(f, *(Uint64*)(buffer                      ), NULL);
+            else
+                matcher.fprintCapture(f, *(Uint64*)(buffer + sizeof(const char*)), *(const char**)buffer);
+        }
     }
     virtual void fprintDebug(RegexMatcher<USE_STRINGS> &matcher, FILE *f)
     {

matcher-optimization.h
@@ -1,7 +1,7 @@
 #include "math-optimization.h"
 
 template <bool USE_STRINGS> // currently implemented only for !USE_STRINGS
-Uint64 RegexMatcher<USE_STRINGS>::matchSymbol_ConstGroup(RegexSymbol *thisSymbol)
+Uint64 RegexMatcher<USE_STRINGS>::matchSymbol_ConstGroup(RegexSymbol *thisSymbol, bool capturing)
 {
     RegexGroup *const group = ((RegexConstGroup*)thisSymbol)->originalGroup;
     RegexPattern **insideAlternative = group->alternatives;
@@ -34,31 +34,39 @@ Uint64 RegexMatcher<USE_STRINGS>::matchSymbol_ConstGroup(RegexSymbol *thisSymbol
             }
         }
     }
-    if (multiple == 0) // don't backtrack when it will make no difference to do so
+    if (multiple == 0 && !capturing) // don't backtrack when it will make no difference to do so
     {
         symbol++;
         return 0;
     }
-    matchSymbol_Character_or_Backref(thisSymbol, multiple, (const char *)NULL);
-    return multiple;
+    if (matchSymbol_Character_or_Backref(thisSymbol, multiple, (const char *)NULL))
+        return multiple;
+    return NON_PARTICIPATING_CAPTURE_GROUP;
 }
 template <bool USE_STRINGS> // currently implemented only for !USE_STRINGS
 void RegexMatcher<USE_STRINGS>::matchSymbol_ConstGroupNonCapturing(RegexSymbol *thisSymbol)
 {
-    matchSymbol_ConstGroup(thisSymbol);
+    matchSymbol_ConstGroup(thisSymbol, false);
 }
 template <bool USE_STRINGS> // currently implemented only for !USE_STRINGS
 void RegexMatcher<USE_STRINGS>::matchSymbol_ConstGroupCapturing(RegexSymbol *thisSymbol)
 {
-    nonMatchHappened = false;
     Uint64 position0 = position;
-    Uint64 multiple = matchSymbol_ConstGroup(thisSymbol);
-    if (multiple != NON_PARTICIPATING_CAPTURE_GROUP && position > position0 && !nonMatchHappened)
+    Uint64 multiple = matchSymbol_ConstGroup(thisSymbol, true);
+    if (multiple != NON_PARTICIPATING_CAPTURE_GROUP)
     {
         Uint backrefIndex = ((RegexConstGroupCapturing*)thisSymbol)->backrefIndex;
 
-        Backtrack_LeaveConstGroupCapturing<USE_STRINGS> *pushStack = stack.template push< Backtrack_LeaveConstGroupCapturing<USE_STRINGS> >();
+        Backtrack_LeaveConstGroupCapturing<USE_STRINGS> *pushStack = stack.template push< Backtrack_LeaveConstGroupCapturing<USE_STRINGS> >(Backtrack_LeaveConstGroupCapturing<USE_STRINGS>::get_size());
         pushStack->backrefIndex = backrefIndex;
+        if (enable_persistent_backrefs)
+        {
+            const char *&dummy = (const char *&)pushStack->buffer;
+            if (!USE_STRINGS)
+                readCapture(backrefIndex, *(Uint64*)(pushStack->buffer                      ), dummy);
+            else
+                readCapture(backrefIndex, *(Uint64*)(pushStack->buffer + sizeof(const char*)), *(const char**)pushStack->buffer);
+        }
 
         Uint64 prevValue = captures[backrefIndex];
         writeCapture(backrefIndex, multiple, (const char *)NULL);
@@ -320,8 +328,8 @@ ALWAYS_INLINE bool RegexMatcher<USE_STRINGS>::staticallyOptimizeGroup(RegexSymbo
 
 template <bool USE_STRINGS>
 template <typename MATCH_TYPE>
-ALWAYS_INLINE bool RegexMatcher<USE_STRINGS>::runtimeOptimize_matchSymbol_Character_or_Backref(RegexSymbol *const thisSymbol, Uint64 const multiple, MATCH_TYPE const repetend)
-// return true if this optimizer function handled the match and the caller should do nothing further
+ALWAYS_INLINE int8 RegexMatcher<USE_STRINGS>::runtimeOptimize_matchSymbol_Character_or_Backref(RegexSymbol *const thisSymbol, Uint64 const multiple, MATCH_TYPE const repetend)
+// return nonzero if this optimizer function handled the match and the caller should do nothing further: +1 if repetend matched at least once, -1 if it matched zero times
 {
     if (optimizationLevel && !thisSymbol->possessive)
     {
@@ -337,17 +345,18 @@ ALWAYS_INLINE bool RegexMatcher<USE_STRINGS>::runtimeOptimize_matchSymbol_Charac
             if (!inrange64(currentMatch, thisSymbol->minCount, MAX_EXTEND(thisSymbol->maxCount)))
             {
                 nonMatch();
-                return true;
+                return -1;
             }
             if (!doesRepetendMatch(repetend, multiple, currentMatch))
             {
                 nonMatch();
-                return true;
+                return -1;
             }
+            int8 matched = currentMatch != 0 ? +1 : -1;
             position     = input - spaceLeft % multiple;
             currentMatch = ULLONG_MAX;
             symbol++;
-            return true;
+            return matched;
         }
         RegexGroup *thisGroup = groupStackTop->group;
         bool afterEndOfGroup = false;
@@ -359,7 +368,7 @@ ALWAYS_INLINE bool RegexMatcher<USE_STRINGS>::runtimeOptimize_matchSymbol_Charac
                 if (!emulate_ECMA_NPCGs)
                 {
                     nonMatch();
-                    return true;
+                    return -1;
                 }
                 subtract = 0;
             }
@@ -367,7 +376,7 @@ ALWAYS_INLINE bool RegexMatcher<USE_STRINGS>::runtimeOptimize_matchSymbol_Charac
             if (subtract > spaceLeft || (spaceLeft - subtract) % multiple != 0)
             {
                 nonMatch();
-                return true;
+                return -1;
             }
         }
         if (nextSymbol && nextSymbol->type==RegexSymbol_Group ||
@@ -404,14 +413,14 @@ ALWAYS_INLINE bool RegexMatcher<USE_STRINGS>::runtimeOptimize_matchSymbol_Charac
                     if (currentMatch < alreadyCaptured)
                     {
                         nonMatch();
-                        return true;
+                        return -1;
                     }
                     currentMatch -= alreadyCaptured;
                     currentMatch /= multiple;
                     if (currentMatch < thisSymbol->minCount)
                     {
                         nonMatch();
-                        return true;
+                        return -1;
                     }
                     if (currentMatch > MAX_EXTEND(thisSymbol->maxCount))
                         currentMatch = MAX_EXTEND(thisSymbol->maxCount);
@@ -420,7 +429,7 @@ ALWAYS_INLINE bool RegexMatcher<USE_STRINGS>::runtimeOptimize_matchSymbol_Charac
                         if (!doesRepetendMatch(repetend, multiple, currentMatch))
                         {
                             nonMatch();
-                            return true;
+                            return -1;
                         }
                     }
                     else
@@ -431,15 +440,16 @@ ALWAYS_INLINE bool RegexMatcher<USE_STRINGS>::runtimeOptimize_matchSymbol_Charac
                             if (currentMatch < thisSymbol->minCount)
                             {
                                 nonMatch();
-                                return true;
+                                return -1;
                             }
                         }
                         pushStack();
                     }
+                    int8 matched = currentMatch != 0 ? +1 : -1;
                     position += currentMatch * multiple;
                     currentMatch = ULLONG_MAX;
                     symbol++;
-                    return true;
+                    return matched;
                 }
             }
             else
@@ -467,27 +477,27 @@ ALWAYS_INLINE bool RegexMatcher<USE_STRINGS>::runtimeOptimize_matchSymbol_Charac
                                         if (totalLength > input || cannotMatch)
                                         {
                                             nonMatch();
-                                            return true;
+                                            return -1;
                                         }
                                         Uint64 target = input - totalLength;
                                         if (position > target)
                                         {
                                             nonMatch();
-                                            return true;
+                                            return -1;
                                         }
                                         Uint64 spaceLeft = target - groupStackTop->position;
                                         currentMatch = spaceLeft / (1 + currentSymbol->minCount);
                                         if (currentMatch < position - groupStackTop->position)
                                         {
                                             nonMatch();
-                                            return true;
+                                            return -1;
                                         }
                                         currentMatch -= position - groupStackTop->position;
                                         currentMatch /= multiple;
                                         if (currentMatch < thisSymbol->minCount)
                                         {
                                             nonMatch();
-                                            return true;
+                                            return -1;
                                         }
                                         if (currentMatch > MAX_EXTEND(thisSymbol->maxCount))
                                             currentMatch = MAX_EXTEND(thisSymbol->maxCount);
@@ -496,7 +506,7 @@ ALWAYS_INLINE bool RegexMatcher<USE_STRINGS>::runtimeOptimize_matchSymbol_Charac
                                             if (!doesRepetendMatch(repetend, multiple, currentMatch))
                                             {
                                                 nonMatch();
-                                                return true;
+                                                return -1;
                                             }
                                         }
                                         else
@@ -507,15 +517,16 @@ ALWAYS_INLINE bool RegexMatcher<USE_STRINGS>::runtimeOptimize_matchSymbol_Charac
                                                 if (currentMatch < thisSymbol->minCount)
                                                 {
                                                     nonMatch();
-                                                    return true;
+                                                    return -1;
                                                 }
                                             }
                                             pushStack();
                                         }
+                                        int8 matched = currentMatch != 0 ? +1 : -1;
                                         position += currentMatch * multiple;
                                         currentMatch = ULLONG_MAX;
                                         symbol++;
-                                        return true;
+                                        return matched;
                                     }
                                 }
                                 break;
@@ -558,13 +569,13 @@ ALWAYS_INLINE bool RegexMatcher<USE_STRINGS>::runtimeOptimize_matchSymbol_Charac
                             if (totalLength > input || cannotMatch)
                             {
                                 nonMatch();
-                                return true;
+                                return -1;
                             }
                             Uint64 target = input - totalLength;
                             if (position > target)
                             {
                                 nonMatch();
-                                return true;
+                                return -1;
                             }
                             Uint64 spaceLeft = target - position;
                             RegexGroup *multiplicationGroup = NULL;
@@ -637,7 +648,7 @@ ALWAYS_INLINE bool RegexMatcher<USE_STRINGS>::runtimeOptimize_matchSymbol_Charac
                             if (currentMatch < thisSymbol->minCount)
                             {
                                 nonMatch();
-                                return true;
+                                return -1;
                             }
                             if (currentMatch > MAX_EXTEND(thisSymbol->maxCount))
                                 currentMatch = MAX_EXTEND(thisSymbol->maxCount);
@@ -646,7 +657,7 @@ ALWAYS_INLINE bool RegexMatcher<USE_STRINGS>::runtimeOptimize_matchSymbol_Charac
                                 if (!doesRepetendMatch(repetend, multiple, currentMatch))
                                 {
                                     nonMatch();
-                                    return true;
+                                    return -1;
                                 }
                             }
                             else
@@ -657,12 +668,13 @@ ALWAYS_INLINE bool RegexMatcher<USE_STRINGS>::runtimeOptimize_matchSymbol_Charac
                                     if (currentMatch < thisSymbol->minCount)
                                     {
                                         nonMatch();
-                                        return true;
+                                        return -1;
                                     }
                                 }
                                 if (currentMatch != (thisSymbol->lazy ? MAX_EXTEND(thisSymbol->maxCount) : thisSymbol->minCount))
                                     pushStack();
                             }
+                            int8 matched = currentMatch != 0 ? +1 : -1;
                             //position = target - spaceLeft % multiple;
                             position += currentMatch * multiple;
                             currentMatch = ULLONG_MAX;
@@ -678,18 +690,18 @@ ALWAYS_INLINE bool RegexMatcher<USE_STRINGS>::runtimeOptimize_matchSymbol_Charac
                                 if (spaceLeft < product)
                                 {
                                     nonMatch();
-                                    return true;
+                                    return -1;
                                 }
                                 enterGroup(multiplicationGroup);
                                 symbol = multiplicationAnchor;
                                 position = input;
                             }
-                            return true;
+                            return matched;
                         }
                     }
                 }
             }
         }
     }
-    return false;
+    return 0;
 }

matcher.cpp
@@ -40,8 +40,6 @@ void RegexMatcher<USE_STRINGS>::nonMatch(NonMatchType type)
         }
     }
 
-    nonMatchHappened = true;
-
     position = groupStackTop->position;
 
     // if any changes are made here, they may need to be duplicated in Backtrack_Commit<USE_STRINGS>::popTo()
@@ -571,19 +569,20 @@ void RegexMatcher<USE_STRINGS>::matchSymbol_Backref(RegexSymbol *thisSymbol)
 
 template <bool USE_STRINGS>
 template <typename MATCH_TYPE>
-void RegexMatcher<USE_STRINGS>::matchSymbol_Character_or_Backref(RegexSymbol *thisSymbol, Uint64 multiple, MATCH_TYPE repetend)
+bool RegexMatcher<USE_STRINGS>::matchSymbol_Character_or_Backref(RegexSymbol *thisSymbol, Uint64 multiple, MATCH_TYPE repetend)
+// return true if repetend was matched at least once
 {
     if (currentMatch == ULLONG_MAX)
     {
-        if (runtimeOptimize_matchSymbol_Character_or_Backref(thisSymbol, multiple, repetend))
-            return;
+        if (char optimized = runtimeOptimize_matchSymbol_Character_or_Backref(thisSymbol, multiple, repetend))
+            return optimized > 0;
         if (thisSymbol->lazy)
         {
             currentMatch = thisSymbol->minCount;
             if (!doesRepetendMatch(repetend, multiple, currentMatch))
             {
                 nonMatch();
-                return;
+                return false;
             }
         }
         else
@@ -595,7 +594,7 @@ void RegexMatcher<USE_STRINGS>::matchSymbol_Character_or_Backref(RegexSymbol *th
                 if (currentMatch < thisSymbol->minCount)
                 {
                     nonMatch();
-                    return;
+                    return false;
                 }
                 if (USE_STRINGS && repetend)
                 {
@@ -603,9 +602,10 @@ void RegexMatcher<USE_STRINGS>::matchSymbol_Character_or_Backref(RegexSymbol *th
                     if (currentMatch < thisSymbol->minCount)
                     {
                         nonMatch();
-                        return;
+                        return false;
                     }
                 }
+                bool matched = currentMatch != 0;
                 if (currentMatch > thisSymbol->minCount)
                     pushStack();
                 if (USE_STRINGS)
@@ -614,7 +614,7 @@ void RegexMatcher<USE_STRINGS>::matchSymbol_Character_or_Backref(RegexSymbol *th
                     position = input - spaceLeft % multiple;
                 currentMatch = ULLONG_MAX;
                 symbol++;
-                return;
+                return matched;
             }
             else
             {
@@ -625,7 +625,7 @@ void RegexMatcher<USE_STRINGS>::matchSymbol_Character_or_Backref(RegexSymbol *th
                     if (currentMatch < thisSymbol->minCount)
                     {
                         nonMatch();
-                        return;
+                        return false;
                     }
                 }
             }
@@ -642,20 +642,21 @@ void RegexMatcher<USE_STRINGS>::matchSymbol_Character_or_Backref(RegexSymbol *th
                 if (USE_STRINGS && thisSymbol->lazy && currentMatch && !doesRepetendMatchOnce(repetend, multiple, currentMatch-1))
                 {
                     nonMatch();
-                    return;
+                    return false;
                 }
+                bool matched = currentMatch != 0;
                 if (currentMatch != (thisSymbol->lazy ? MAX_EXTEND(thisSymbol->maxCount) : thisSymbol->minCount))
                     pushStack();
                 position     = neededMatch;
                 currentMatch = ULLONG_MAX;
                 symbol++;
-                return;
+                return matched;
             }
         }
         if (thisSymbol->lazy)
         {
             nonMatch();
-            return;
+            return false;
         }
     try_next_match:
         if (thisSymbol->lazy)
@@ -663,7 +664,7 @@ void RegexMatcher<USE_STRINGS>::matchSymbol_Character_or_Backref(RegexSymbol *th
             if (currentMatch == MAX_EXTEND(thisSymbol->maxCount))
             {
                 nonMatch();
-                return;
+                return false;
             }
             currentMatch++;
         }
@@ -672,7 +673,7 @@ void RegexMatcher<USE_STRINGS>::matchSymbol_Character_or_Backref(RegexSymbol *th
             if (currentMatch == thisSymbol->minCount)
             {
                 nonMatch();
-                return;
+                return false;
             }
             currentMatch--;
         }

commit 7fda09e10a96cf45ab9f5cc2f9dbdd6b134cc967

Fix constant-group optimization crash
I had forgotten the reason popForAtomicCapture()'s return value can be negative, and incorrectly copied its being negative using Backtrack_LoopGroup as a template.

Bug introduced in 8b7cdbdfb811504128d9dafbe1748f43d5bce06b

matcher.h
@@ -1028,7 +1028,7 @@ protected:
     }
     virtual int popForAtomicCapture(RegexMatcher<USE_STRINGS> &matcher)
     {
-        return -1;
+        return 1;
     }
     virtual captureTuple popForAtomicForwardCapture(RegexMatcher<USE_STRINGS> &matcher, Uint captureNum)
     {